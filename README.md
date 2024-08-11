987abd9923eba163b60800a8477989e72ab4b977
mbm_1607 <muhammadkhan1607@gmail.com>
2024-08-11 16:55:04 +0500

    :zap: Update code to make it easier to run

--- README.md
+++ README.md
@@ -1,20 +1,39 @@
 # Introduction
 
-Azimuthal equidistant projection created with the holy Kabbah, created using cartopy.
+Azimuthal equidistant projection centered on the holy Kabbah, created using cartopy.
 
 Cartopy is a Python package designed for geospatial data processing in order to produce maps and
 other geospatial data analyses.
-Cartopy makes use of the powerful PROJ.4, NumPy and Shapely libraries and includes a programmatic
-interface built on top of Matplotlib for the creation of publication quality maps.
+
+Cartopy makes use of the powerful **PROJ.4**, **NumPy** and **Shapely** libraries and includes a
+programmatic interface built on top of Matplotlib for the creation of publication quality maps.
 
 ## Development
 
-You need to install [Install Cartopy Dependencies](https://scitools.org.uk/cartopy/docs/latest/installing.html#required-dependencies) before this tool can be used.
-
-After that install all python dependencies
+First we setup a virtual environment to avoid conflicts with other python packages.
 
 ```bash
-pipenv install
+python -m venv venv
+```
+
+Activate the virtual environment
+
+```bash
+source venv/bin/activate
+```
+
+After that we install the required packages
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running the code
+
+To run the code, simply execute the following command
+
+```bash
+python main.py
 ```
 
 ## Projection Around Kabbah
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-import matplotlib.pyplot as plt
 import cartopy
+import matplotlib.pyplot as plt
 
-my_lon, my_lat = 39.826206, 21.422487
-plt.figure()
-ax = plt.axes(
-    projection=cartopy.crs.AzimuthalEquidistant(
-        central_latitude=my_lat, central_longitude=my_lon
+
+def main():
+    kabah_longitude, kabah_latitude = 39.826206, 21.422487
+    plt.figure()
+    ax = plt.axes(
+        projection=cartopy.crs.AzimuthalEquidistant(
+            central_latitude=kabah_latitude, central_longitude=kabah_longitude
+        )
     )
-)
 
-ax.stock_img()
-ax.add_feature(cartopy.feature.LAND)
-ax.add_feature(cartopy.feature.OCEAN)
-ax.add_feature(cartopy.feature.COASTLINE)
-ax.add_feature(cartopy.feature.BORDERS, linestyle="dotted")
-ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
-ax.add_feature(cartopy.feature.RIVERS)
+    ax.stock_img()
+    ax.add_feature(cartopy.feature.LAND)
+    ax.add_feature(cartopy.feature.OCEAN)
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.BORDERS, linestyle="dotted")
+    ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
+    ax.add_feature(cartopy.feature.RIVERS)
 
-ax.set_global()
-ax.gridlines()
+    ax.set_global()
+    ax.gridlines()
 
-plt.plot(
-    my_lon,
-    my_lat,
-    color="#ED2939",
-    marker="o",
-    transform=cartopy.crs.Geodetic(),
-)
+    plt.plot(
+        kabah_longitude,
+        kabah_latitude,
+        color="#ED2939",
+        marker="o",
+        transform=cartopy.crs.Geodetic(),
+    )
 
-plt.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
